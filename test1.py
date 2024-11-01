73d2e11467684e18d1b9d848c266e61e94cad83c
“Ecstaky” <1666888655@qq.com>
2024-11-01 12:36:44 +0800

    i

--- test1.py
+++ test1.py
@@ -1,8 +1,8 @@
-def search():
-    num1,num2.sort()
-    a=len(num1)
-    b=len(num2)
-    print(num1[int(a/2)])
-    print(num2[int(b/2)])
-    pass
-search()
+def search_medium_number():
+    num1=[1,2,5,6,9,7]
+    num2=[2,5,4,4,8,7,5,6]
+    new_num=num1+num2
+    new_num.sort()
+    lenth=len(new_num)
+    print(new_num[lenth-1])
+search_medium_number()
