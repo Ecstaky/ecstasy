8e7232be31b4ea22953267ba945df1aabb0ea2a9
“Ecstaky” <1666888655@qq.com>
2024-11-02 13:04:03 +0800

    i

---
+++ second.test1.py
@@ -0,0 +1,21 @@
+
+a=(1,2,3,4,5,6)
+b=(25641,15,1,15,4,6)
+c=a+b
+print(c)
+
+
+dict= {'John':'98','Lina':'80'}
+print(dict['John'])
+dict['Li hua']=75
+print(dict.keys())
+dict['Lina']=70
+del dict['John']
+print(dict)
+
+
+a=set([1,3,5,7,9])
+b=set([1,2,3,4,5,6,])
+
+print(a&b)
+print(a|b)
